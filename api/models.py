8917b27d92664b62de7da1c857916bd98f2ddda9
jayz25 <60577923+jayz25@users.noreply.github.com>
2022-08-29 17:51:43 +0530

    Global Stats Integration (#1)
    - Added Global Stats Model
    - Added API views for stats

--- api/models.py
+++ api/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class Paragraph(models.Model):
     paragraph = models.TextField()
 
+class GlobalStats(models.Model):
+    user = models.CharField(max_length=20)
+    words_per_minute = models.IntegerField()
+    characters_per_minute = models.IntegerField()
+    accuracy = models.IntegerField()
+
+
--- api/urls.py
+++ api/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('addPara/', views.add_paragraph, name='addPara'),
     path('updatePara/<str:id_to_update>/', views.update_paragraph, name='updatePara'),
     path('deletePara/<str:id_to_delete>/', views.delete_paragraph, name='deletePara'),
+    path('getStats/', views.get_stats, name='getStats'),
+    path('addStat/', views.add_stat, name='addStat')
 ]
