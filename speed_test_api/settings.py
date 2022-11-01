0a75310294a64f9898e34cb94428d1d1075ab69e
Jayesh <patiljayesh026@gmail.com>
2022-11-02 02:47:12 +0530

    Heroku Deployment IX

--- speed_test_api/settings.py
+++ speed_test_api/settings.py
@@ -166,5 +166,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
+    'https://speed-test-ten.vercel.app',
     'https://localhost:3000','https://127.0.0.1:3000', 'http://localhost:3000', 'http://127.0.0.1:3000', 'https://speedtype-api.herokuapp.com'
 ]
